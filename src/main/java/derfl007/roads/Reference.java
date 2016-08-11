e96b77eff78f8585b2be88b209f15142839cb6eb
derfl007 <flokiflott@gmail.com>
2016-08-11 16:25:45 +0200

    fixed bounding box sidewalk curbs

--- src/main/java/derfl007/roads/Reference.java
+++ src/main/java/derfl007/roads/Reference.java
@@ -12,5 +12,14 @@ public class Reference {
 	public static final String SERVER_PROXY_CLASS = "derfl007.roads.proxy.CommonProxy";
 
 	public static final AxisAlignedBB ROAD_BLOCK_AABB = new AxisAlignedBB(0.0D, 0.0D, 0.0D, 1.0D, 0.82D, 1.0D);
-	public static final AxisAlignedBB ROAD_SIGN_AABB = new AxisAlignedBB(0.0D, 0.0D, 0.0D, 1.0D, 1.0D, 1.0D);
+	public static final AxisAlignedBB SC_INNER_WEST_AABB = new AxisAlignedBB(0.75D, 1.0D, 0.75D, 1.0D, 1.0625D, 1.0D);
+	public static final AxisAlignedBB SC_INNER_EAST_AABB = new AxisAlignedBB(0.0D, 1.0D, 0.0D, 0.25D, 1.0625D, 0.25D);
+	public static final AxisAlignedBB SC_INNER_SOUTH_AABB = new AxisAlignedBB(0.75D, 1.0D, 0.0D, 1.0D, 1.0625D, 0.25D);
+	public static final AxisAlignedBB SC_INNER_NORTH_AABB = new AxisAlignedBB(0.0D, 1.0D, 0.75D, 0.25D, 1.0625D, 1.0D);
+	public static final AxisAlignedBB SC_STRAIGHT_WEST_AABB = new AxisAlignedBB(0.0D, 1.0D, 0.0D, 0.25D, 1.0625D, 1);
+	public static final AxisAlignedBB SC_STRAIGHT_EAST_AABB = new AxisAlignedBB(0.75D, 1.0D, 0.0D, 1.0D, 1.0625D, 1.0D);
+	public static final AxisAlignedBB SC_STRAIGHT_SOUTH_AABB = new AxisAlignedBB(0.0D, 1.0D, 0.0D, 1.0D, 1.0625D,
+			0.25D);
+	public static final AxisAlignedBB SC_STRAIGHT_NORTH_AABB = new AxisAlignedBB(0.0D, 1.0D, 0.75D, 1.0D, 1.0625D,
+			1.0D);
 }
