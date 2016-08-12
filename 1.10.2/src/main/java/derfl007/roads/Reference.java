93a7dd554969d4c87a98f0795623c6c587c0ec22
derfl007 <flokiflott@gmail.com>
2016-08-12 20:42:29 +0200

    updated reference and build.gradle to version 1.0.1

--- 1.10.2/src/main/java/derfl007/roads/Reference.java
+++ 1.10.2/src/main/java/derfl007/roads/Reference.java
@@ -5,14 +5,14 @@ import net.minecraft.util.math.AxisAlignedBB;
 public class Reference {
 	public static final String MOD_ID = "roads";
 	public static final String MOD_NAME = "Road Mod";
-	public static final String VERSION = "1.0";
+	public static final String VERSION = "1.0.1";
 	public static final String ACCEPTED_VERSIONS = "[1.10.2]";
 
 	public static final String CLIENT_PROXY_CLASS = "derfl007.roads.proxy.ClientProxy";
 	public static final String SERVER_PROXY_CLASS = "derfl007.roads.proxy.CommonProxy";
 
 	public static final AxisAlignedBB ROAD_BLOCK_AABB = new AxisAlignedBB(0.0D, 0.0D, 0.0D, 1.0D, 0.82D, 1.0D);
-	
+
 	public static final AxisAlignedBB SC_INNER_WEST_AABB = new AxisAlignedBB(0.75D, 1.0D, 0.75D, 1.0D, 1.0625D, 1.0D);
 	public static final AxisAlignedBB SC_INNER_EAST_AABB = new AxisAlignedBB(0.0D, 1.0D, 0.0D, 0.25D, 1.0625D, 0.25D);
 	public static final AxisAlignedBB SC_INNER_SOUTH_AABB = new AxisAlignedBB(0.75D, 1.0D, 0.0D, 1.0D, 1.0625D, 0.25D);
